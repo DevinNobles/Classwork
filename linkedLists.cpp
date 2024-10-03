3c32bb2ae94e3a42a65ccba6f93a1ca49b387823
DevinNobles <144385943+DevinNobles@users.noreply.github.com>
2024-10-03 14:07:10 -0400

    Update linkedLists.cpp

--- linkedLists.cpp
+++ linkedLists.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
-
 using namespace std;
 
-// Step 1: Define the node structure
-struct nodeType {
-    int info;            // This holds the data (can be any data type)
-    nodeType *link;      // Pointer to the next node in the list
+// Template for linked list node
+template <typename T>
+struct Node {
+    T info;            // The data part
+    Node<T>* link;     // Pointer to the next node
 };
 
-// Step 2: Declare the head of the linked list
-nodeType *head = nullptr;
+// Template for linkedListType class
+template <typename T>
+class linkedListType {
+private:
+    Node<T>* head;     // Pointer to the first node in the list
+
+public:
+    // Constructor: initialize the head pointer to nullptr
+    linkedListType() {
+        head = nullptr;
+    }
+
+    // Function to insert a node at the beginning of the list
+    void insertFirst(T newItem) {
+        Node<T>* newNode = new Node<T>; // Allocate memory for the new node
+        newNode->info = newItem;        // Set the data
+        newNode->link = head;           // Make new node point to the current head
+        head = newNode;                 // Update head to point to the new node
+    }
+
+    // Function to display the linked list
+    void displayList() {
+        Node<T>* current = head;        // Start at the head
+        while (current != nullptr) {
+            cout << current->info << " -> ";
+            current = current->link;    // Move to the next node
+        }
+        cout << "nullptr" << endl;      // End of the list
+    }
+};
 
 int main() {
-    // Step 3: Create nodes and link them
-    nodeType *node1 = new nodeType; // Allocate memory for the first node
-    node1->info = 10;               // Assign value to the node
-    node1->link = nullptr;          // Initially, the link points to null
+    // Create a linked list of integers
+    linkedListType<int> intList;
 
-    nodeType *node2 = new nodeType; // Allocate memory for the second node
-    node2->info = 20;
-    node2->link = nullptr;          // Last node will point to null
+    // Insert some integers
+    intList.insertFirst(10);
+    intList.insertFirst(20);
+    intList.insertFirst(30);
 
-    // Step 4: Link the nodes
-    node1->link = node2;            // First node points to the second
-
-    // Step 5: Set the head pointer to the first node
-    head = node1;
-
-    // Step 6: Traverse and print the linked list
-    nodeType *current = head;       // Start at the head
-  
-    while (current != nullptr) {
-        cout << current->info << " -> ";
-        current = current->link;
-    }
-    cout << "nullptr" << endl;      // To show the end of the list
+    // Display the list
+    intList.displayList();
 
     return 0;
 }
