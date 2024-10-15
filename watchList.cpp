d3c1c006643cf1357adbb8d7c723505ab4834f45
DevinNobles <144385943+DevinNobles@users.noreply.github.com>
2024-10-15 15:06:33 -0400

    .

---
+++ watchList.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout <<"Hello Guys" << endl;
+    return 0;
+}
+
+
+
+
+
+// You will create a menu-driven program where the user can:
+
+// Add a Movie to the Beginning of the Watchlist
+// The user will input the movie's title and the movie will be inserted at the beginning of the watchlist.
+
+// Add a Movie to the End of the Watchlist
+// Similar to option 1, but the movie will be added to the end of the watchlist.
+
+// Remove a Movie by Title
+// The user will input the title of the movie to be removed. If the movie is found in the list, it will be deleted. 
+
+// Display All Movies in the Watchlist
+// Display all movies currently in the watchlist in the order they were added.
+
+// Exit the Program
+// Exits the program.
